e1942ffee2aebc1237d4722e7183cdda91446827
Bob Qian <s3840234@student.rmit.edu.au>
2022-08-15 17:57:04 +1000

    Initial Files for App

---
+++ src/main/java/edu/septv/homy1/Homy1Application.java
@@ -0,0 +1,18 @@
+package edu.septv.homy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class Homy1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homy1Application.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/edu/septv/homy1/Homy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.septv.homy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homy1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

aa4048ecb129430577615c337745a98d75322d7e
Connor <conerpen@gmail.com>
2022-10-18 14:21:49 +1100

    Tests for booking

---
+++ BackEnd/src/test/java/com/example/homy1/model/BookingTest.java
@@ -0,0 +1,18 @@
+package com.example.homy1.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BookingTest {
+    //Nothing to test everything is just setters and getters
+    @Test
+    @DisplayName("")
+    public void function_return_condition()  {
+
+    }
+
+}
